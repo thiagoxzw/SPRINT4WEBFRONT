9c9ee870ccbd9017f2a882b6df0bd50bd111e61b
thiagoxzw <thiagoslima123@gmail.com>
2026-09-23 20:10:33 +0000

    chore: atualiza o import do contexto no useAuth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { AuthContext } from '../context/authContext'
+import { AuthContext } from '../context/contexts'
 
 export function useAuth() {
   const context = useContext(AuthContext)

ede24fc8563a31a3475080eae88e1ddb55679597
thiagoxzw <thiagoslima123@gmail.com>
2026-09-23 19:48:03 +0000

    feat: adiciona os dados da equipe NextStage

---
+++ src/data/team.js
@@ -0,0 +1,7 @@
+export const team = [
+  { name: 'JOÃO VITOR BATISTA DE MATTOS', rm: 'RM 571389', role: 'Desenvolvimento & Front-End', color: 'blue' },
+  { name: 'MATHEUS AKIRA ASO', rm: 'RM 572830', role: 'Tech Lead & Desenvolvimento', color: 'pink' },
+  { name: 'KELVYN LUKAS JUNQUEIRA DA SILVA', rm: 'RM 572950', role: 'Desenvolvimento & UX', color: 'green' },
+  { name: 'THIAGO SOUZA DE LIMA', rm: 'RM 568732', role: 'Product & Pesquisa', color: 'teal' },
+  { name: 'GUILERME KOOJI KUBOTA', rm: 'RM 570541', role: 'Design & Prototipação', color: 'orange' },
+]
